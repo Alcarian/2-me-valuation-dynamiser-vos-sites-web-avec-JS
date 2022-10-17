bc466f0aec80b7928a777fc0b75b30db20daa1ce
Alcarian <floa2@msn.com>
2022-10-17 11:24:56 +0200

    script button newgame

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ const btnRules = document.querySelector(".btn-rules");
 const rulesPage = document.querySelector(".rules");
 const overlay1 = document.querySelector(".overlay");
 const btnCloseRules = document.querySelector(".closeRules");
+const btnNewGame = document.querySelector(".newgame");
+const player0 = document.querySelector(".player-0");
+const player1 = document.querySelector(".player-1");
+const score0 = document.getElementById("score-0");
+const score1 = document.getElementById("score-1");
 
 // End declare variables
 
@@ -24,3 +29,25 @@ btnCloseRules.addEventListener("click", closeRules);
 overlay1.addEventListener("click", closeRules);
 
 // End Rules page
+
+// Start New Game
+
+btnNewGame.addEventListener("click", function () {
+  document.querySelector(`.player-${activePlayer}`).classList.remove("winner");
+
+  if (!player0.classList.contains("player-active")) {
+    player0.classList.toggle("player-active");
+    player1.classList.toggle("player-active");
+  }
+
+  score0.textContent = 0;
+  score1.textContent = 0;
+
+  diceImgEl.classList.add("hidden");
+
+  totalScore = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+});
+
+// End New Game
