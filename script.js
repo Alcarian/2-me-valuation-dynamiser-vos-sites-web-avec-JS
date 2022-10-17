69100e0297f31f658ac2ea3541e0aeaaecfdeca2
Alcarian <floa2@msn.com>
2022-10-17 12:36:06 +0200

    change name img

--- script.js
+++ script.js
@@ -9,8 +9,32 @@ const player0 = document.querySelector(".player-0");
 const player1 = document.querySelector(".player-1");
 const score0 = document.getElementById("score-0");
 const score1 = document.getElementById("score-1");
+const btnRoll = document.querySelector(".rolldice");
+const current0 = document.getElementById("current-0");
+const current1 = document.getElementById("current-1");
+const btnHold = document.querySelector(".hold");
+const diceImg = document.querySelector(".dice");
 
 // End declare variables
+
+// Starting conditions
+
+score0.textContent = 0;
+score1.textContent = 0;
+
+let totalScore = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+
+let nextUserTurn = function () {
+  currentScore = 0;
+  document.getElementById(`current-${activePlayer}`).textContent = currentScore;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0.classList.toggle("player-active");
+  player1.classList.toggle("player-active");
+  play2();
+};
+// End conditions
 
 // Start Rules page
 
@@ -43,7 +67,7 @@ btnNewGame.addEventListener("click", function () {
   score0.textContent = 0;
   score1.textContent = 0;
 
-  diceImgEl.classList.add("hidden");
+  diceImg.classList.add("hidden");
 
   totalScore = [0, 0];
   currentScore = 0;
@@ -51,3 +75,45 @@ btnNewGame.addEventListener("click", function () {
 });
 
 // End New Game
+
+// Start Game
+// Rolling Dice
+
+btnRoll.addEventListener("click", function () {
+  const randomNumber = Math.trunc(Math.random() * 6 + 1);
+
+  diceImg.classList.remove("hidden");
+
+  diceImg.src = `dice-${randomNumber}.png`;
+
+  if (randomNumber !== 1) {
+    currentScore = currentScore + randomNumber;
+    document.getElementById(`current-${activePlayer}`).textContent =
+      currentScore;
+    play1();
+  } else {
+    nextUserTurn();
+  }
+});
+
+// End Rolling Dice
+// Start Hold score
+
+btnHold.addEventListener("click", function () {
+  totalScore[activePlayer] = totalScore[activePlayer] + currentScore;
+  document.getElementById(`score-${activePlayer}`).textContent =
+    totalScore[activePlayer];
+
+  if (totalScore[activePlayer] >= 100) {
+    document.querySelector(`.player-${activePlayer}`).classList.add("winner");
+    play3();
+    currentScore = 0;
+    document.getElementById(`current-${activePlayer}`).textContent =
+      currentScore;
+  } else {
+    nextUserTurn();
+  }
+});
+
+// End Hold score
+// End Game
